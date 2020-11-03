7c36a7d26e92c09510976ae77b22208cec71e3d2
Zeyu Miao <zeyumiao@Zeyus-MacBook-Air.local>
2020-11-03 10:57:23 -0500

    completed error-handling

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,19 +1,22 @@
 def handle_error_by_throwing_exception():
-    raise Exception("There's an error.")
+    raise ValueError("There's an error.")
 
 
 def handle_error_by_returning_none(input_data):
     try:
         return int(input_data)
-    except:
+    except ValueError:
         return None
 
 def handle_error_by_returning_tuple(input_data):
     try:
         return (True, int(input_data))
-    except:
+    except ValueError:
         return (False, "Input is not Int")
 
 
-def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+def filelike_objects_are_closed_on_exception(filelike_object):    
+    try:
+        filelike_object.do_something()
+    finally:
+        filelike_object.close()
